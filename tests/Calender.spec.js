9442b2719c5d922ff0969e2fabd1134fca1cde1c
Nitesh Kumar <nitesh.hope@gmail.com>
2024-07-23 15:03:45 +0530

    code update

---
+++ tests/Calender.spec.js
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test'
+
+test.only('Calender handling ', async ({ page }) => {
+
+    const month = 6;
+    const year = 2027
+    const day = 15
+    let value = [];
+
+    const exptectedDate = [month, day, year]
+    await page.goto('https://rahulshettyacademy.com/seleniumPractise/#/offers')
+
+    await page.locator('.react-date-picker__inputGroup').click()
+    await page.locator('.react-calendar__navigation__label__labelText--from').click()
+    await page.locator('.react-calendar__navigation__label__labelText--from').click()
+    await page.getByText(year).click()
+    await page.locator('.react-calendar__year-view__months__month').nth(month - 1).click()
+    await page.locator('//abbr[text()=' + day + ']').click()
+    const inputs = page.locator('.react-date-picker__inputGroup__input')
+    //console.log("count of inputs - " + await inputs.count())
+    for (let index = 0; index < await inputs.count(); ++index) {
+        const value = await inputs.nth(index).getAttribute('value')
+        console.log("value - " + value)
+        //the below code wwill not work because it 
+        expect(parseInt(value)).toEqual(exptectedDate[index])
+        //  expect(parseInt(value)).toEqual(parseInt(exptectedDate[index]))
+
+
+    }
+
+
+
+})
--- tests/specialLocator.spec.js
+++ tests/specialLocator.spec.js
@@ -12,6 +12,24 @@ test.only('speical locator', async ({ page }) => {
     await page.getByLabel('Check me out if you Love IceCreams!').click()
     await page.getByLabel('Employed').check()
     await page.getByLabel('Gender').selectOption('Female')
+    // if we have tag name is placeholder we can use getByPlaceholder()
+    await page.getByPlaceholder('Password').fill('1234abc')
+
+    // getByRole()- based on type which you look 
+    await page.getByRole("button", { name: 'Submit' }).click()
+
+    //  getBytext() -it will look for text     
+    await page.getByText('Success! The Form has been submitted successfully!.').isVisible()
+
+    await page.getByRole("link", { name: "Shop" }).click()
+
+    // chainig of locator 
+    // we use css the use filter() which internally use getByText()
+    //then use getByRole() here we have only one button so we pass simply  button without name
+    await page.locator('app-card').filter({ hasText: 'Nokia Edge' }).getByRole('button').click()
+
+
+
 
 
 
